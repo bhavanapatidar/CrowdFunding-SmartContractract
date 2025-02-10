25278ff55712448c8f807e923409670ea3efd588
bhavanapatidar <bhavanapatidar2708@gmail.com>
2025-02-10 16:32:26 +0530

    Contribution Function

--- contracts/crowdfunding.sol
+++ contracts/crowdfunding.sol
@@ -23,4 +23,38 @@ contract CrowdFunding{
     uint public target;
     uint public raisedAmount;
     uint public noOfContributers;
+
+    constructor(uint _target, uint _deadline ){
+        target = _target;
+        deadline = block.timestamp+_deadline;
+        minimumContribution=10 wei;
+        manager = msg.sender;
+
+    }
+
+    modifier onlyManager(){
+        require(msg.sender==manager, "you are not manager");
+        _;
+    }
+    function createRequests(string calldata _description, address payable _recipient, uint _value ) public onlyManager{ 
+        Request storage newRequest = requests[numRequests];
+        numRequests++;
+        newRequest.description = _description;
+        newRequest.recipient = _recipient;
+        newRequest.value = _value;
+        newRequest.completed = false;
+        newRequest.noOfVoters = 0;  
+    }
+
+    function contribution() public payable{
+        require(block.timestamp<deadline, "Deadline has passed");
+        require(msg.value>=minimumContribution,"payment should be more than minimum contribution");
+
+        if(contributers[msg.sender]==0){
+            noOfContributers++;
+        }
+        contributers[msg.sender]+=msg.value;
+        raisedAmount+=msg.value;
+
+    }
 }
