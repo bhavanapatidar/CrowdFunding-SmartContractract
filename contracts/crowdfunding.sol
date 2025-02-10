2c5ce273150576861e1375f2ec42e429444ab1dc
bhavanapatidar <bhavanapatidar2708@gmail.com>
2025-02-11 00:11:21 +0530

    All functions

--- contracts/crowdfunding.sol
+++ contracts/crowdfunding.sol
@@ -57,4 +57,32 @@ contract CrowdFunding{
         raisedAmount+=msg.value;
 
     }
+
+    function getContractBalance() public view returns (uint){
+        return address(this).balance;
+    }
+
+    function refund() public{
+        require(block.timestamp>deadline && raisedAmount<target, "You are not elgible for refund");
+        require(contributers[msg.sender]>0, "You are not contributor");
+        payable(msg.sender).transfer(contributers[msg.sender]);
+        contributers[msg.sender]=0;
+    }
+
+    function voteRequest(uint _requestNo) public{
+        require(contributers[msg.sender]>0,"You are not a contributer");
+        Request storage thisRequest = requests[_requestNo];
+        require(thisRequest.voters[msg.sender]==false,"You have already voted");
+        thisRequest.voters[msg.sender]=true;
+        thisRequest.noOfVoters++;
+    }
+
+    function makePayment(uint _requestNo) public onlyManager{
+        require(raisedAmount >= target, "target is not reached");
+        Request storage thisRequest = requests[_requestNo];
+        require(thisRequest.completed == false, "the request has been completed");
+        require(thisRequest.noOfVoters>noOfContributers/2, "majority does not support the request");
+        thisRequest.recipient.transfer(thisRequest.value);
+        thisRequest.completed = true;
+    }
 }
